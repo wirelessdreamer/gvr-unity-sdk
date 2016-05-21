6f3c799623af2ead07cc260aa40b64f4aba90800
Nathanael Anderson <wirelessdreamer@gmail.com>
2016-05-21 01:05:12 -0400

    Initial Commit, provides a wrapper that should activate the gaze trigger for any imagined input. The goal is to provide a fast and simple way to get a physical controller running in a demo.

---
+++ GoogleVR/DemoScenes/HeadsetDemo/ZapperInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZapperInput : MonoBehaviour {
+    // Use this for initialization
+    GazeInputModule gazeInput;
+
+    void Start () {
+        gazeInput = FindObjectOfType<GazeInputModule>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+        System.Array values = System.Enum.GetValues(typeof(KeyCode));
+        foreach (KeyCode code in values)
+        {
+            if (Input.GetKeyDown(code))
+            {
+                gazeInput.HandleTrigger();
+            }
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            print("trigger pressed");
+            gazeInput.HandleTrigger();
+        }
+
+        if (Input.GetKeyDown(KeyCode.JoystickButton4))
+        {
+            print("reload pressed");
+        }
+
+    }
+}
